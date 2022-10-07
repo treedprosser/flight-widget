a14275452097b9bc16904251efc451d52f5bc805
Trevor Prosser <treedprosser@gmail.com>
2022-10-07 16:59:17 -0600

    upload files

---
+++ src/components/Table.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TableHead from './TableHead'
+import TableBody from './TableBody'
+
+const Table = () => {
+  return (
+    <table>
+      <TableHead />
+      <TableBody />
+    </table>
+  )
+}
+
+export default Table
---
+++ src/components/TableRow.jsx
@@ -0,0 +1,16 @@
+import TableCell from './TableCell'
+
+const TableRow = ({ flight }) => {
+  const words = Object.values(flight)
+
+  return (
+    <tr>
+      <td>✈️</td>
+      {words?.map((word, _index) => (
+        <TableCell key={_index} word={word} />
+        ))}
+    </tr>
+  )
+}
+
+export default TableRow
